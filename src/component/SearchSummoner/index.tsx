9ceb77ebcabee8f8d4ee7dcec5fbf8c7d94b1292
AimHo <jjhwhwnsgh@gmail.com>
2020-08-02 10:49:33 +0900

    [update] 소환사 검색 최근 검색 리스트 popover input 클릭 시 노출되고 영역 외 클릭하면 닫히도록 수정

--- src/component/SearchSummoner/index.tsx
+++ src/component/SearchSummoner/index.tsx
@@ -1,6 +1,7 @@
 import { uniqBy, findIndex } from 'lodash';
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useState, useLayoutEffect, useRef } from 'react';
 
+import useOutsideRef from '../../hooks/useOutsideRef';
 import { getItem, setItem } from '../../utils/localStorage';
 
 import { Container, SearchInput, SearchImgBtn } from './style';
@@ -8,8 +9,15 @@ import { SearchSummonerProps, ISearchItem } from './types';
 import SearchPopover from './SearchPopover';
 
 const SearchSummoner = (props: SearchSummonerProps) => {
+  // popover state
+  const [popoverActive, setPopoverActive] = useState(false);
+  const popoverRef = useRef(null);
+  useOutsideRef(popoverRef, () => setPopoverActive(false));
+
+  // localStorage 검색 state
   const [recentSearches, setRecentSearch] = useState([] as ISearchItem[]);
 
+  // load localStorage 검색 데이터
   useLayoutEffect(() => {
     const recentSummoners = getItem('recentSummoners') as ISearchItem[];
     setRecentSearch((value) => (recentSummoners ? recentSummoners : value));
@@ -25,7 +33,7 @@ const SearchSummoner = (props: SearchSummonerProps) => {
 
     const items = [...recentSearches];
     const targetIndex = findIndex(recentSearches, ['name', searchItem.name]);
-
+    // 최근 검색 결과, 즐겨찾기 localStorage 등록
     if (targetIndex !== -1) {
       const result = { ...recentSearches[targetIndex], isRecent: true };
       items.splice(targetIndex, 1);
@@ -40,12 +48,16 @@ const SearchSummoner = (props: SearchSummonerProps) => {
   };
 
   return (
-    <Container style={{ width: props.width ? props.width : '260px' }}>
+    <Container
+      ref={popoverRef}
+      style={{ width: props.width ? props.width : '260px' }}
+    >
       <SearchInput
         type="search"
         placeholder="소환사 검색, 챔피언.."
         value={props.value}
         onChange={(e) => props.onChange(e.target.value)}
+        onFocus={() => setPopoverActive(true)}
       />
       <SearchImgBtn
         onClick={() => onSearch()}
@@ -54,7 +66,7 @@ const SearchSummoner = (props: SearchSummonerProps) => {
       />
 
       <SearchPopover
-        active={!props.value}
+        active={!props.value && popoverActive}
         onSearch={props.onSearch}
         recentSearches={recentSearches}
         setRecentSearch={(items: ISearchItem[]) => {
